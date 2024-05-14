f610fae6d8fb991487639a11ad9df154e2fdd03b
Luis-Miguelj <luismiguel.dev01@gmail.com>
2024-05-14 09:49:34 -0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,20 @@ interface UserProps{
   token: [{id: string, userId: string}]
 }
 export default async function Home() {
-  const response = await fetch(`https://app-three-ashen.vercel.app/api/user/663c03a6ebd5fca6f44e6658`)
+  const response = await fetch(`https://app-three-ashen.vercel.app/api/user/`)
   const data = await response.json()
 
   console.log(data)
   return (
     <div>
-      {/* {
-        data.user.map((user: UserProps, index:number) => (
+      {
+        data.usuarios.map((user: UserProps, index:number) => (
           <div key={index}>
             <h1>{user.username}</h1>
             <p>{user.email}</p>
           </div>
         ))
-      } */}
+      }
       <h1>Home Page</h1>
     </div>
   );
