280059fb73b45611ea8b2d931582ddb19b45492a
Luis-Miguelj <luismiguel.dev01@gmail.com>
2024-05-14 09:11:03 -0300

    add cors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,19 @@ interface UserProps{
   token: [{id: string, userId: string}]
 }
 export default async function Home() {
-  const response = await fetch(`app-three-ashen.vercel.app/api/user`)
-  const data = await response.json()
+  // const response = await fetch(`app-three-ashen.vercel.app/api/user`)
+  // const data = await response.json()
 
   return (
     <div>
-      {
+      {/* {
         data.user.map((user: UserProps, index:number) => (
           <div key={index}>
             <h1>{user.username}</h1>
             <p>{user.email}</p>
           </div>
         ))
-      }
+      } */}
       <h1>Home Page</h1>
     </div>
   );
